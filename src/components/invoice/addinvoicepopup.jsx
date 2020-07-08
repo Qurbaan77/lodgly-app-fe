5a24bc586c2ffbee4e4ac376cd9c38a4d7657765
fullstack_websul <developer@websultanate.com>
2020-07-08 10:49:58 +0530

    changes in add invoice

--- src/components/invoice/addinvoicepopup.jsx
+++ src/components/invoice/addinvoicepopup.jsx
@@ -40,6 +40,7 @@ import { Row, Col } from 'antd';
 import { Collapse } from 'antd';
 import delete_icon from '../../assets/images/menu/delete-icon-red.png';
 import moment from 'moment';
+import { userInstance } from '../../axios/axiosconfig';
 
 const { Panel } = Collapse;
 
@@ -53,58 +54,111 @@ const AdInvoicePopup = () => {
   const [email, setEmail] = useState('');
   const [address, setAddress] = useState('');
   const [vatId, setVatId] = useState(null);
+  const [pricePanel, setPricePanel] = useState([1]);
   const [quantity, setQuantity] = useState(null);
   const [price, setPrice] = useState(null);
   const [amount, setAmount] = useState(null);
   const [discountType, setDiscountType] = useState('%');
   const [discount, setDiscount] = useState(null);
   const [itemTotal, setItemTotal] = useState(null);
+  const [quantityCopy, setQuantityCopy] = useState([]);
+  const [priceCopy, setPriceCopy] = useState([]);
+  const [amountCopy, setAmountCopy] = useState([]);
+  const [discountCopy, setDiscountCopy] = useState([]);
+  const [itemTotalCopy, setItemTotalCopy] = useState([]);
+  const [one, setOne] = useState(false);
+  const [two, setTwo] = useState(false);
+  const [three, setThree] = useState(false);
   const [grandTotal, setGrandToal] = useState(null);
   const { Option } = Select;
 
   const { MonthPicker, RangePicker } = DatePicker;
 
-  const handleFinish = (values) => {
+  const handleFinish = async (values) => {
     values.date = date;
     values.deliveryDate = deliveryDate;
     values.dueDate = dueDate;
     values.time = time;
+    pricePanel.map((el) => {
+      console.log(values[el])
+    })
     console.log(values);
+
+    const res = await userInstance.post('/createInvoice', values);
   };
 
-  const handlePrice = (e) => {
-    const d = parseFloat(e.target.value);
-    console.log(typeof d);
+
+  const handleQuantity = (e, ele) => {
+    const d = e.target.value;
+    console.log(d);
+    setQuantity(d);
+    setQuantityCopy(quantityCopy => [...quantityCopy, d]);
+
+  }
+  console.log(quantity);
+  console.log(quantityCopy);
+
+  const handlePrice = (e, ele) => {
+    console.log(e.target.value);
+    const d = e.target.value;
     setPrice(d);
-    const d1 = d * quantity;
-    setAmount(d1);
-  };
+    setPriceCopy(priceCopy => [...priceCopy, d]);
+    setAmount(d * quantity);
+    setAmountCopy(amountCopy => [...amountCopy, quantityCopy[ele - 1] * d]);
 
-  const handleDiscount = (e) => {
+  };
+  console.log(price);
+  console.log(quantityCopy);
+  console.log(priceCopy)
+
+  const handleDiscount = (e, ele) => {
     const d = e.target.value;
     setDiscount(d);
+    setDiscountCopy(discountCopy => [...discountCopy, d]);
     if (discountType === '%') {
+      console.log(amountCopy[ele - 1]);
       setItemTotal(amount - (amount * d) / 100);
+      setItemTotalCopy(itemTotalCopy => [...itemTotalCopy, amountCopy[ele - 1] - (amountCopy[ele - 1] * d) / 100]);
     } else {
       setItemTotal(amount - d);
+      setItemTotalCopy(itemTotalCopy => [...itemTotalCopy, amountCopy[ele - 1] - d]);
     }
   };
 
-  const handleDiscountType = (value) => {
+
+  const handleDiscountType = (value, ele) => {
     setDiscountType(value);
-    if (discount) {
+    if (discount && discountCopy.length) {
       console.log('a');
       if (value === '%') {
         console.log('b');
+        console.log(amountCopy[ele - 1] - (amountCopy[ele - 1] * discountCopy[ele - 1]) / 100);
         setItemTotal(amount - (amount * discount) / 100);
+        setItemTotalCopy(itemTotalCopy => [...itemTotalCopy, amountCopy[ele - 1] - (amountCopy[ele - 1] * discountCopy[ele - 1]) / 100]);
       } else {
         setItemTotal(amount - discount);
+        setItemTotalCopy(itemTotalCopy => [...itemTotalCopy, amountCopy[ele - 1] - discountCopy[ele - 1]]);
+        console.log(amountCopy[ele - 1] - discountCopy[ele - 1]);
         console.log('c');
       }
     }
   };
 
-  console.log(quantity, price, amount, discountType, discount, itemTotal);
+  console.log(itemTotalCopy)
+  let i = 1;
+
+  const addMorePanel = () => {
+    setOne(false);
+    console.log('gsfg');
+    i++;
+    setPricePanel([...pricePanel, i]);
+  }
+
+  const removePanel = (ele) => {
+    const oldarray = [...pricePanel];
+    oldarray.pop();
+    setPricePanel([...oldarray]);
+  }
 
   return (
     <Form name='basic' onFinish={handleFinish}>
@@ -231,71 +285,98 @@ const AdInvoicePopup = () => {
         </Col>
       </Row>
 
-      <div className='additional-fields'>
-        <Row style={{ alignItems: 'center' }}>
-          <Col span={6}>
-            <Form.Item name='itemDescription' label='Item Description'>
-              <Input />
-            </Form.Item>
-          </Col>
+      {pricePanel.map((ele, i) => {
+        let d;
+        switch (i) {
+          case 1:
+            d = one;
+            break;
+          case 2:
+            d = two;
+            break;
+          case 3:
+            d = three;
+            break;
+          default:
+            d = false;
+        }
+        {/* d.replace(/^"(.*)"$/, '$1'); */ }
+        console.log(d);
+        return (
+          <div className='additional-fields'>
+            <Row style={{ alignItems: 'center' }}>
+              <Col span={6}>
+                <Form.Item name={[ele, 'itemDescription']} label='Item Description'>
+                  <Input />
+                </Form.Item>
+              </Col>
 
-          <Col span={2}>
-            <Form.Item name='Quantity' label='Qty.'>
-              <Input
-                value={quantity}
-                onChange={(e) => setQuantity(parseFloat(e.target.value))}
-              />
-            </Form.Item>
-          </Col>
+              <Col span={2}>
+                <Form.Item name={[ele, 'quantity']} label='Qty.'>
+                  <Input
+                    // value={quantity}
+                    onBlur={(e) => handleQuantity(e, ele)}
+                  />
+                </Form.Item>
+              </Col>
 
-          <Col span={3}>
-            <Form.Item name='price' label='Price'>
-              <Input value={price} onChange={(e) => handlePrice(e)} />
-            </Form.Item>
-          </Col>
+              <Col span={3}>
+                <Form.Item name={[ele, 'price']} label='Price'>
+                  <Input
+                    // value={price} 
+                    onBlur={(e) => handlePrice(e, ele)} />
+                </Form.Item>
+              </Col>
 
-          <Col span={3}>
-            <div className='amount-field'>
-              <p>{amount}</p>
-            </div>
-          </Col>
+              <Col span={3}>
+                <div className='amount-field'>
+                  <p>{amountCopy[ele - 1]}</p>
+                </div>
+              </Col>
 
-          <Col span={2} className='label-hidden'>
-            <Form.Item name='discountType' label='Discount Type'>
-              <Select
-                placeholder='Discount type'
-                onSelect={(value) => handleDiscountType(value)}
-                defaultValue='%'
-              >
-                <Select.Option value='€'>€</Select.Option>
-                <Select.Option value='%'>%</Select.Option>
-              </Select>
-            </Form.Item>
-          </Col>
+              <Col span={2} className='label-hidden'>
+                <Form.Item name={[ele, 'discountType']} label='Discount Type'>
+                  <Select
+                    placeholder='Discount type'
+                    onSelect={(value) => handleDiscountType(value, ele)}
+                    defaultValue='%'
+                  >
+                    <Select.Option value='€'>€</Select.Option>
+                    <Select.Option value='%'>%</Select.Option>
+                  </Select>
+                </Form.Item>
+              </Col>
 
-          <Col span={3}>
-            <Form.Item name='discount' label='Discount'>
-              <Input value={discount} onChange={(e) => handleDiscount(e)} />
-            </Form.Item>
-          </Col>
+              <Col span={3}>
+                <Form.Item name={[ele, 'discount']} label='Discount'>
+                  <Input
+                    // value={discount} 
+                    onBlur={(e) => handleDiscount(e, ele)} />
+                </Form.Item>
+              </Col>
 
-          <Col span={3}>
-            <div className='amount-field'>
-              <p>{itemTotal}</p>
-            </div>
-          </Col>
+              <Col span={3}>
+                <div className='amount-field' key={i}>
+                  <p>{itemTotalCopy[ele - 1]}
+                  </p>
+                </div>
+              </Col>
 
-          <Col span={2} className='deleteicon'>
-            <Form.Item>
-              <img src={delete_icon} alt='delete' />
-            </Form.Item>
-          </Col>
-        </Row>
-      </div>
+              <Col span={2} className='deleteicon'>
+                <Form.Item>
+                  <img src={delete_icon} alt='delete' onClick={() => removePanel(ele)} />
+                </Form.Item>
+              </Col>
+            </Row>
+          </div>
+        );
+      })}
+
+
 
       <Row style={{ alignItems: 'center' }}>
         <Col span={12}>
-          <div className='additional-add-guest'>
+          <div className='additional-add-guest' onClick={addMorePanel}>
             <PlusOutlined /> Add additional guest
           </div>
         </Col>
