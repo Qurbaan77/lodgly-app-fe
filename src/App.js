4b33383a79ea07fbccb211dca8d221137f5d5c72
ashrafwbst <er.ashrafkamal@gmail.com>
2020-09-05 20:38:05 +0530

    fix issues

--- src/App.js
+++ src/App.js
@@ -35,9 +35,9 @@ import CreateSeasonRates from './components/property/createseasonrates';
 import GuestList from './components/guests/guests';
 import CompanyList from './components/guests/company';
 
-import Overview from './components/property/overview';
-import Location from './components/property/location';
-import Photos from './components/property/photos';
+// import Overview from './components/property/overview';
+// import Location from './components/property/location';
+// import Photos from './components/property/photos';
 
 import { PrivateRoute, LoginRoute } from './Routes/PrivateRoute';
 import {
@@ -478,9 +478,9 @@ const App = () => {
                       <LoginRoute exact path="/" component={() => <Login />} />
                     )}
 
-                    <Route exact path="/overview" component={() => <Overview />} />
+                    {/* <Route exact path="/overview" component={() => <Overview />} />
                     <Route exact path="/location" component={() => <Location />} />
-                    <Route exact path="/photos" component={() => <Photos />} />
+                    <Route exact path="/photos" component={() => <Photos />} /> */}
 
                     <Route component={PageNotFound} />
 
